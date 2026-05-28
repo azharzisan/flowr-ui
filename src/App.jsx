7f1b3668a7119e7f12a51d20a285f73d8df8cb62
Azhar Zisan <azharzisanxtr@gmail.com>
2026-05-28 22:13:45 +0100

    app.jsx sections added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import './App.css'
+import "./App.css";
+import Navbar from "@/sections/Navbar";
+import Hero from "@/sections/Hero";
+import { useRef, useEffect, useState } from "react";
+import About from "./sections/About";
+import Choose from "./sections/Choose";
 
 function App() {
+  const navRef = useRef(null);
+  const [navHeight, setNavHeight] = useState(0);
+
+  useEffect(() => {
+    if (navRef.current) {
+      setNavHeight(navRef.current.offsetHeight);
+    }
+  }, []);
+
   return (
     <>
+      <Navbar ref={navRef} />
+      <section style={{ paddingTop: navHeight }}>
+        <Hero />
+      </section>
+      <section>
+        <About />
+      </section>
+      <section>
+        <Choose />
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
