2370a5410499b3ab379e979aff3ef699731cf42a
Azhar Zisan <azharzisanxtr@gmail.com>
2026-05-30 22:09:11 +0100

    defauly nav anf footer for every page

---
+++ src/layouts/Default.jsx
@@ -0,0 +1,29 @@
+import Footer from "@/sections/Footer";
+import Navbar from "@/sections/Navbar";
+import { Outlet } from "react-router-dom";
+import React from "react";
+import { useRef, useEffect, useState } from "react";
+
+const Default = () => {
+  const navRef = useRef(null);
+  const [navHeight, setNavHeight] = useState(0);
+
+  useEffect(() => {
+    if (navRef.current) {
+      setNavHeight(navRef.current.offsetHeight);
+    }
+  }, []);
+  return (
+    <>
+      <div>
+        <Navbar ref={navRef} />
+        <section style={{ paddingTop: navHeight }}>
+          <Outlet />
+        </section>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Default;
