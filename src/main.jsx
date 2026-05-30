8cfb064f807d5c268da3dd8ffd8fc00865505a35
Azhar Zisan <azharzisanxtr@gmail.com>
2026-05-30 22:18:30 +0100

    dynamic router layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Default from './layouts/Default'
+import Catagory from './pages/Catagory'
+import ProductPage from './pages/ProductPage'
+import Subscription from './pages/Subscription'
+
+const router = createBrowserRouter([{
+  element: <Default />,
+  children : [
+    {path: '/', element: <App/>},
+    {path: '/catagory', element: <Catagory/>},
+    {path: '/product', element: <ProductPage/>},
+    {path: '/subscription', element: <Subscription/>},
+  ]
+}])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}/>
 )
